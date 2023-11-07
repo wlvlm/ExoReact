3b92da224bd50f1f0f33537debb0873952b783ed
William Vuillaume <williampgm77@gmail.com>
2023-11-07 11:57:35 +0100

    Ajout footer

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import './App.css'
+
+function Footer(){
+    return (
+        <footer>
+            <div class="footer">
+                <ul>
+                    <li><a href="#">Nous trouver
+                        </a></li>
+                    <li><a href="#">Nous contacter
+                        </a></li>
+                    <li><a href="#">Nous suivre
+                        </a></li>
+                </ul>
+            </div>
+            <p>&copy; La Piscine 2023. Tous droits réservés</p>
+        </footer>
+    )
+}
+
+export default Footer;
