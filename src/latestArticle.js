056a74673df35edd791727eda2ff6fc7647a6476
William Vuillaume <williampgm77@gmail.com>
2023-11-07 13:02:01 +0100

    Fin de l'exo 3

---
+++ src/latestArticle.js
@@ -0,0 +1,24 @@
+import './App.css'
+
+function LatestArticle() {
+    return (
+        <main className='App-articleContainer'>
+            <article className='App-article'>
+                <h3 className='Article-name'>Titre de l'article</h3>
+                <p className='Article-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eos, libero natus autem quam voluptate est consectetur soluta esse ad aperiam obcaecati similique in incidunt rerum, eum asperiores dicta qui vitae magnam eius sapiente. Rerum optio voluptate sapiente in voluptatibus tenetur autem ipsam perferendis consectetur possimus? Nesciunt voluptatum nulla odio at. Iusto repellendus aliquid maiores ratione, veniam praesentium delectus reprehenderit ad<a href="#">test</a> dolorem corporis aliquam dicta dolorum sint voluptates voluptatibus architecto? Quas ex animi aspernatur laudantium expedita consequatur debitis eligendi, molestias dolores sed labore necessitatibus iste sunt velit ad ut id blanditiis? Enim voluptatum quae eius quod provident accusantium maxime repellendus error laborum consectetur in suscipit, adipisci dolore reprehenderit ratione blanditiis laboriosam vel delectus neque! Eveniet earum et eum illum? Itaque perferendis dicta illum, eum suscipit, omnis eius, voluptas labore odit voluptate nisi fugit. A nostrum, aliquam dignissimos eaque voluptatum modi ut laboriosam voluptatibus dolore nihil velit commodi autem rem iste!</p>
+            </article>
+
+            <article className='App-article'>
+                <h3 className='Article-name'>Titre de l'article</h3>
+                <p className='Article-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eos, libero natus autem quam voluptate est consectetur soluta esse ad aperiam obcaecati similique in incidunt rerum, eum asperiores dicta qui vitae magnam eius sapiente. Rerum optio voluptate sapiente in voluptatibus tenetur autem ipsam perferendis consectetur possimus? Nesciunt voluptatum nulla odio at. Iusto repellendus aliquid maiores ratione, veniam praesentium delectus reprehenderit ad dolorem corporis aliquam dicta dolorum sint voluptates voluptatibus architecto? Quas ex animi aspernatur laudantium expedita consequatur debitis eligendi, molestias dolores sed labore necessitatibus iste sunt velit ad ut id blanditiis? Enim voluptatum quae eius quod provident accusantium maxime repellendus error laborum consectetur<a href="#">test</a> in suscipit, adipisci dolore reprehenderit ratione blanditiis laboriosam vel delectus neque! Eveniet earum et eum illum? Itaque perferendis dicta illum, eum suscipit, omnis eius, voluptas labore odit voluptate nisi fugit. A nostrum, aliquam dignissimos eaque voluptatum modi ut laboriosam voluptatibus dolore nihil velit commodi autem rem iste!</p>
+            </article>
+
+            <article className='App-article'>
+                <h3 className='Article-name'>Titre de l'article</h3>
+                <p className='Article-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium eos, libero natus autem quam voluptate est consectetur soluta esse ad aperiam obcaecati similique in incidunt rerum, eum asperiores dicta qui vitae magnam eius sapiente. Rerum optio voluptate sapiente in voluptatibus tenetur autem ipsam perferendis consectetur possimus? Nesciunt voluptatum nulla odio at. Iusto repellendus aliquid maiores ratione, veniam praesentium delectus reprehenderit ad dolorem corporis aliquam dicta dolorum sint voluptates voluptatibus architecto? Quas ex animi aspernatur laudantium <a href="#">test</a>expedita consequatur debitis eligendi, molestias dolores sed labore necessitatibus iste sunt velit ad ut id blanditiis? Enim voluptatum quae eius quod provident accusantium maxime repellendus error laborum consectetur in suscipit, adipisci dolore reprehenderit ratione blanditiis laboriosam vel delectus neque! Eveniet earum et eum illum? Itaque perferendis dicta illum, eum suscipit, omnis eius, voluptas labore odit voluptate nisi fugit. A nostrum, aliquam dignissimos eaque voluptatum modi ut laboriosam voluptatibus dolore nihil velit commodi autem rem iste!</p>
+            </article>
+        </main>
+    )
+}
+
+export default LatestArticle;
